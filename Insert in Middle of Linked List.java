6ec8c6c523871d86225466b32a1ba858dfe839ed
mukesh111 <66948644+mukesh61112@users.noreply.github.com>
2022-05-20 11:48:06 +0530

    Create Insert in Middle of Linked List.java

---
+++ Insert in Middle of Linked List.java
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        
+        while(t-->0){
+            int n = sc.nextInt();   
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++){
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            
+            //display(head);
+            Solution obj = new Solution();
+            obj.insertInMid(head, key);
+            display(head);
+            System.out.println();
+        }
+    }
+    
+    public static void display(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node insertInMid(Node head, int data){
+        //Insert code here, return the head of modified linked list
+        
+        Node newNode=new Node(data);
+        Node temp=head;
+        
+        
+        int count=1;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+            count++;
+        }
+      
+        Node check=head;
+        if(count%2==0)
+           count=count/2;
+        else
+           count=count/2+1;
+        while(count>1)
+        {
+            check=check.next;
+            --count;
+        }
+        newNode.next=check.next;
+        check.next=newNode;
+        
+        return head;
+    }
+}
